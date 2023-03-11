693b7c25f0bd76812d448f767efa9851f6b8808a
Siddharth Bisht <siddharthbisht31@gmail.com>
2023-03-12 02:00:37 +0530

    Update UniqueCharacters.js

--- arrays/UniqueCharacters.js
+++ arrays/UniqueCharacters.js
@@ -1,21 +1,18 @@
 /**
- * Implement an algorithm to determine if a string has all unique characters 
+ * Implement an algorithm to determine if a string has all unique characters
  */
 
-
-const solution=(s)=>{
-    let hashMap={};
-    let flag= false;
-    for(let i=0;i<s.length;i++){
-        if(hashMap[`${s.charAt(i)}`]){
-            flag= true;
-            break;
-        }
-        else{
-            hashMap[`${s.charAt(i)}`]=true;
-        }
+const solution = (s) => {
+  let hashMap = {};
+  let flag = false;
+  for (let i = 0; i < s.length; i++) {
+    if (hashMap[`${s.charAt(i)}`]) {
+      flag = true;
+      break;
+    } else {
+      hashMap[`${s.charAt(i)}`] = true;
     }
-    return flag;
-}
-console.log(
-    solution('sidhart'));xx  
+  }
+  return flag;
+};
+console.log(solution("sidhart"));
