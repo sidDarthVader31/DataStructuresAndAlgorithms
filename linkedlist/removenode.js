2f969b736d120495f7a57ec81805fab6b4c25f8f
Siddharth Bisht <siddharthbisht31@gmail.com>
2023-03-15 19:35:08 +0530

    Create removenode.js

---
+++ linkedlist/removenode.js
@@ -0,0 +1,44 @@
+/*
+* you are given the head of a linked list.
+
+Remove every node which has a node with a strictly greater value anywhere to the right side of it.
+
+Return the head of the modified linked list.
+
+ 
+
+Example 1:
+
+
+Input: head = [5,2,13,3,8]
+Output: [13,8]
+Explanation: The nodes that should be removed are 5, 2 and 3.
+- Node 13 is to the right of node 5.
+- Node 13 is to the right of node 2.
+- Node 8 is to the right of node 3.
+Example 2:
+
+Input: head = [1,1,1,1]
+Output: [1,1,1,1]
+Explanation: Every node has value 1, so no nodes are removed.
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var removeNodes = function (head) {
+  if (!head) return null;
+
+  head.next = removeNodes(head.next);
+
+  if (head.next && head.val < head.next.val) return head.next;
+
+  return head;
+};
+//26->13->3->8
+//
