789352e4da6bab31c993082a96263a972f947b57
Siddharth Bisht <siddharthbisht31@gmail.com>
2023-03-27 21:34:05 +0530

    Update quicksort.js

--- recursion/quicksort.js
+++ recursion/quicksort.js
@@ -6,14 +6,32 @@
 /**
  * @param{number[]}arr
  **/
-const quickSort = (arr) => {
-  const pivot = arr.length - 1;
-  const pivotValue = arr[pivot];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > pivot) {
-      let temp = arr[i];
-      arr[i] = pivot;
-      arr[pivot] = temp;
-    }
+const sort = (arr) => {
+  return quickSort(arr, 1, arr.length);
+};
+const quickSort = (arr, left, right) => {
+  if (left < right) {
+    let q = partition(arr, left, right);
+    quickSort(arr, left, q);
+    quickSort(arr, q + 1, right);
+  } else {
+    return arr;
   }
 };
+const partition = (arr, left, right) => {
+  let x = arr[right - 1];
+  let i = left - 1;
+
+  for (let j = left - 1; j < right; j++) {
+    if (arr[j] <= x) {
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      i = i + 1;
+    }
+  }
+  console.log(`array`, arr);
+  return i - 1;
+};
+
+console.log(`sortL`, sort([5, 3, 1, 4, 6, 2]));
