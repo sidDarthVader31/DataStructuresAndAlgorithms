092a8ddfed2f987497eb80ab9662952c5daf96fa
Siddharth Bisht <siddharthbisht31@gmail.com>
2023-04-24 21:31:53 +0530

    Update positionofElement.js

--- recursion/positionofElement.js
+++ recursion/positionofElement.js
@@ -51,21 +51,46 @@ const searchRangeOptimized = (nums, target) => {
   let rightPosition = -1;
   let position = -1;
 
-  return [leftPosition, rightPosition];
+  return binarySearch(nums, left, right, target, -1, -1);
 };
 
-const binarySearch = (nums, left, right, target) => {
+const binarySearch = (
+  nums,
+  left,
+  right,
+  target,
+  leftPostion,
+  rightPosition
+) => {
+  let leftPos = leftPostion;
+  let rightPos = rightPosition;
   if (right >= left) {
     let mid = Math.floor((left + right) / 2);
-    if (nums[mid] == target) {
-      return mid;
+    if (nums[mid] == target && leftPostion == -1) {
+      //first encounter of target element
+
+      leftPos = mid;
+      rightPos = mid;
+      binarySearch(nums, left, mid - 1, target, mid, mid);
+      return binarySearch(nums, mid + 1, right, target, mid, mid);
+    } else if (nums[mid] == target && leftPostion > -1) {
+      //target has been found before
+      if (mid < leftPostion) {
+        leftPos = mid;
+        binarySearch(nums, left, mid - 1, target, mid, rightPosition);
+        return binarySearch(nums, mid + 1, right, target, mid, rightPosition);
+      } else if (mid > rightPosition) {
+        rightPos = mid;
+        binarySearch(nums, left, mid - 1, target, leftPostion, mid);
+        return binarySearch(nums, mid + 1, right, target, leftPostion, mid);
+      }
     } else if (nums[mid] < target) {
-      return binarySearch(nums, mid + 1, right, target);
+      return binarySearch(nums, mid + 1, right, target, leftPos, rightPos);
     } else {
-      return binarySearch(nums, left, mid - 1, target);
+      return binarySearch(nums, left, mid - 1, target, leftPos, rightPos);
     }
   }
-  return -1;
+  return [leftPos, rightPos];
 };
-console.log(`binary search`, binarySearch([5, 7, 7, 8, 9, 10], 0, 5,8));
-//console.log(`result:::`, searchRangeOptimized([5, 7, 7, 8, 8, 10], 8));
+//console.log(`binary search`, binarySearch([5, 7, 7, 8, 9, 10], 0, 5,8));
+console.log(`result:::`, searchRangeOptimized([5, 7, 7, 8, 8, 10], 8));
