2501406d4d1ef0910a7127440a8a51ce51d6d5ba
Siddharth Bisht <siddharthbisht31@gmail.com>
2023-06-01 22:20:56 +0530

    Create maxDepth.js

---
+++ trees/maxDepth.js
@@ -0,0 +1,20 @@
+let maxDepth = 0;
+
+const getMaxDepth = (root) => {
+  if (root == null) {
+    return maxDepth;
+  }
+  const maximumDepth = recursion(root, 1);
+};
+
+const recursion = (root, height) => {
+  if (root == null) {
+    return height;
+  }
+  let leftHeight = recursion(root.left, height);
+  let rightHeight = recursion(root.right, height);
+  let maxOfTwo = Math.max(leftHeight, rightHeight);
+  if (maxOfTwo > maxDepth) {
+    maxDepth = maxOfTwo;
+  }
+};
