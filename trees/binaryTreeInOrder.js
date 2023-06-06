1974cd7fcd3c8f287a970283508b99d78abc0b9b
Siddharth Bisht <siddharthbisht31@gmail.com>
2023-06-06 22:17:40 +0530

    Create binaryTreeInOrder.js

---
+++ trees/binaryTreeInOrder.js
@@ -0,0 +1,41 @@
+/**
+* Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+let result = [];
+var inorderTraversal = function (root) {
+  if (root === null) {
+    return [];
+  } else
+    return [
+      ...inorderTraversal(root.left),
+      root.val,
+      ...inorderTraversal(root.right),
+    ];
+};
