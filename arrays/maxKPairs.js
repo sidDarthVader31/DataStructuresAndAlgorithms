0c3c6dfc84b99f2534f312603c8e12e188fe05f2
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-06-08 12:34:05 +0530

    add max number of k pairs solution

---
+++ arrays/maxKPairs.js
@@ -0,0 +1,45 @@
+/**
+ *You are given an integer array nums and an integer k.
+
+In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+
+Return the maximum number of operations you can perform on the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4], k = 5
+Output: 2
+Explanation: Starting with nums = [1,2,3,4]:
+- Remove numbers 1 and 4, then nums = [2,3]
+- Remove numbers 2 and 3, then nums = []
+There are no more pairs that sum up to 5, hence a total of 2 operations.
+Example 2:
+
+Input: nums = [3,1,3,4,3], k = 6
+Output: 1
+Explanation: Starting with nums = [3,1,3,4,3]:
+- Remove the first two 3's, then nums = [1,4,3]
+There are no more pairs that sum up to 6, hence a total of 1 operation.
+ * link : https://leetcode.com/problems/max-number-of-k-sum-pairs/description/?envType=study-plan-v2&envId=leetcode-75
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxOperations = function(nums, k) {
+    let map = {};
+    let count = 0;
+    for(let i = 0;i< nums.length; i++){
+       if(map[`${nums[i]}`] && map[`${nums[i]}`] > 0){
+           count++;
+           map[`${nums[i]}`] = map[`${nums[i]}`] -1;
+        }
+        else{
+            let requiredNumber = k- nums[i];
+            map[requiredNumber] = (map[requiredNumber] || 0) + 1;
+        }
+        
+    }
+    return count;  
+};
