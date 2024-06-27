9aaea812cf86b856c897d5017cc1563e6b84f16f
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-06-27 11:27:53 +0530

    add spells and potions optmimized solution

--- arrays/spellsAndPotions.ts
+++ arrays/spellsAndPotions.ts
@@ -44,3 +44,32 @@ function successfulPairs(spells: number[], potions: number[], success: number): 
     }
     return output;
 };
+
+//nlogn
+function successfulPairsOptimized(spells: number[], potions: number[], success: number): number[] {
+    
+    let output : number[]=[];
+    const sortedPotions = potions.sort(function(a, b) {
+        return a - b;
+        })
+
+        for(let i = 0;i< spells.length;i++){
+            let spell = spells[i];
+            let left = 0;
+            let right = sortedPotions.length-1;
+            while(left<=right){
+                let mid = Math.floor((left+right)/2);
+                if(sortedPotions[mid]* spell < success){
+                    left = mid+1;
+                }
+                else{
+                    right = mid-1
+                }
+            }
+            output.push(potions.length - left)
+        }
+        return output;
+};
+
+
+
