82966fc748b2e6c474a08cb2c3f22a632768daaa
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-07-08 10:25:45 +0530

    add search in matrix optimal solution

--- arrays/search2dMatrix.js
+++ arrays/search2dMatrix.js
@@ -33,3 +33,29 @@ const search = (nums, target)=>{
     }
     return false;
 }
+
+// log (m*n) time complexity
+var searchMatrixOptimized = function(matrix, target) {
+    let mid = -1;
+    let left = 0;
+    let right = matrix.length-1;
+
+    while(left<= right){
+         mid = Math.floor((left+right)/2);
+        let midLength = matrix[mid].length;
+        if(target <= matrix[mid][midLength-1] && target >= matrix[mid][0]){
+            break;
+        }
+        else if (target < matrix[mid][0]){
+            right = mid-1;
+        }
+        else{
+            left = mid+1;
+        }
+    }
+    if(mid == -1){
+        return false;
+    }
+    return search(matrix[mid], target)
+};
+
