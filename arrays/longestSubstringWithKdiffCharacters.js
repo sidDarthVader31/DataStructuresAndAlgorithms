48e57c65ad6e193a06e7c5a09b1ac60399deb77e
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-10-08 13:23:42 +0530

    add solution for longest substring with k distinct characters

--- arrays/longestSubstringWithKdiffCharacters.js
+++ arrays/longestSubstringWithKdiffCharacters.js
@@ -1,27 +1,28 @@
 longestSubstringWithKdistinct = (str, k) => {
   let left = 0;
   let map = {};
-  let longestSubstring = -Infinity;
-  for(let right = 0 ;right< str.length;right++){
-    let char = str.charAt(right);
-    if(map[char]){
-      map[char] = map[char] +1 ;
+  let maxLength = 0;
+  for(let end = 0;end< str.length;end++){
+    let endChar = str.charAt(end);
+    if(!map[endChar]){
+      map[endChar] = 1;  
     }
     else{
-      map[char] = 1 
+      map[endChar] = map[endChar] + 1;
     }
-    while(Object.keys(map).length > k ){
-    map[str.charAt(left)] = map[str.charAt(left)] - 1;
-    if(map[str.charAt(left)] == 0){
-    delete map[str.charAt(left)]
+    //start slinding window 
+    while(Object.keys(map).length > k) {
+      let leftChar = str.charAt(left);
+      map[leftChar] = map[leftChar] - 1;
+      if(map[leftChar] <= 0){
+        delete map[leftChar];
+      }
+      left ++; 
     }
-    longestSubstring = Math.max(longestSubstring, right-left +1); 
-      left ++;
-    }
+    maxLength = Math.max(maxLength, (end-left+1));
   }
-  return longestSubstring == -Infinity ? 0 : longestSubstring
+  return maxLength
 }
-
 console.log(longestSubstringWithKdistinct("araaci", 2))//4, The longest substring with no more than '2' distinct characters is "araa".
 console.log(longestSubstringWithKdistinct("araaci", 1))//2, The longest substring with no more than '1' distinct characters is "aa".
 console.log(longestSubstringWithKdistinct("cbbebi", 3))//5, The longest substrings with no more than '3' distinct characters are "cbbeb" & "bbebi".
